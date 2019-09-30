f21f8abf743a832dbe7f7ccaaf734e913bc18317
Rishab Manocha <rishab.manocha@outlook.com>
2019-09-30 14:10:18 -0700

    Removed the image-webpack-loader as it is not compatible with IOS devices. The images don't show up on IOS

--- webpack.config.js
+++ webpack.config.js
@@ -39,34 +39,10 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
             }, {
                 test: /\.(webp|ttf|eot|svg|png|jpe?g|gif|ico)$/,
-                loaders: [
-                    {
+                loaders: [{
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]'
-                        }
-                    }, {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
                         }
                     }
                 ]
