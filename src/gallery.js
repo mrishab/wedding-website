7860e9bd3197139e49a59ef308b728c4176a0dc7
Rishab Manocha <rishab.manocha@outlook.com>
2019-09-30 14:17:52 -0700

    Added the loading of the gallery images with webpack.

--- src/gallery.js
+++ src/gallery.js
@@ -5,6 +5,13 @@ import './assets/css/common.css';
 import './assets/css/gallery.css';
 
 export default class Gallery extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        // Importing all the gallery images
+        require.context("./assets/images/gallery", false, /\.(png|webp|jpe?g|svg)$/);
+    }
+
     render() {
         return (<div className="gallery grid col-3 col-sm-2">
             {this.createImageTiles()}
