880c6923785b4f293af9a0a669f19c4410cfc556
Rishab Manocha <rishab.manocha@outlook.com>
2020-01-12 19:36:04 -0800

    Support multiple paragraphs in story cards
    By using a newline character, '\n' each story card now contains multiple paragraphs

--- src/story-board.js
+++ src/story-board.js
@@ -5,19 +5,31 @@ import "./assets/css/text.css";
 import "./assets/css/story-board.css";
 
 export default class StoryBoard extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.data = this.props.story;
+    }
+
     render() {
-        let data = this.props.story;
         return (<div className="story-board row-2 col-2 transition transition-transform transition-fast" >
             <div className="content grid">
                 <div className="text-container">
-                    <h2>{data.title}</h2>
-                    <div className="capsule text-center margin-auto bg-yellow">{data.date}</div>
+                    <h2>{this.data.title}</h2>
+                    <div className="capsule text-center margin-auto bg-yellow">{this.data.date}</div>
                 </div>
                 <div className="img-container">
-                    <img src={data.image} className="img-fluid" alt="A Memory" />
+                    <img src={this.data.image} className="img-fluid" alt="A Memory" />
                 </div>
-                <p className="description">{data.description}</p>
+                <div className="description">
+                    {this.getParagraphs()}
+                </div>
             </div>
         </div>);
+    }
+
+    getParagraphs() {
+        const paraTexts = this.data.description.split("\n");
+
+        return Array.from(paraTexts, (text, index) => <p key={index}>{text}</p>);
     }
 }
