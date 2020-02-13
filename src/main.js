37f20b55eda06fe0432c20642d576d8b5b4d923c
Rishab Manocha <rishab.manocha@outlook.com>
2020-02-12 20:45:21 -0800

    Fixed the name bug. Ampersand was missing spaces.

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ export default class Main extends React.PureComponent {
             <CurveText className="margin-bottom-negative-50 text-upper font-size-subtitle margin-v25px bold" radius={360}>We are getting married</CurveText>
 
             <img className="icon height-150px invert-color" src={RingImage} alt="A white ring with diamond" />
-            <p className="cursive font-size-huge margin-10">{this.state.groomName} <span className="text-yellow">&amp;</span>{this.state.brideName}</p>
+            <p className="cursive font-size-huge margin-10">{this.state.groomName} <span className="text-yellow">&amp;</span> {this.state.brideName}</p>
 
             <p className="font-size-normal letter-spacing-3px">Countdown to the date</p>
             <CountdownClock date={this.props.date} />
